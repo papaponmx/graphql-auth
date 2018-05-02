10e08b92343c8212ac232ea41313ae3d74ce5bc8
Jaime Rios <jaimemadrigalrios@gmail.com>
2018-05-01 22:11:41 -0500

    Added mutation.

--- client/components/header/index.js
+++ client/components/header/index.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import query from '../../queries/current_user';
 import { Link } from 'react-router';
+import mutation from '../../mutations/logout';
 
 class Header extends Component {
+  onLogoutClick() {
+
+  }
+
   renderButtons(){
     const { loading, user } = this.props.data
 
@@ -13,7 +18,8 @@ class Header extends Component {
     }
 
     if (user) {
-      return <li>Logout</li>;
+      return (
+        <li><a onClick={this.onLogoutClick.bind(this)}>Logout</a></li>);
     } else {
       return (
         <div>
@@ -42,4 +48,6 @@ class Header extends Component {
   }
 }
 
-export default graphql(query)(Header);
+export default graphql(mutation)(
+  graphql(query)(Header)
+);
