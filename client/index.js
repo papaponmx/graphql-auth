8b46ececb1856821f30c62b4157e17f1096006f7
Jaime Rios <jaimemadrigalrios@gmail.com>
2018-05-01 19:47:51 -0500

    Added Apollo client to front end.

--- client/index.js
+++ client/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApollloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApollloClient({
+  dataIdFromObject: o => o.id
+});
 
 const Root = () => {
   return (
-    <div>
-      Auth Starter
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        Auth Starter
+      </div>
+    </ApolloProvider>
   );
 };
 
