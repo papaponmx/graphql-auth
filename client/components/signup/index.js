c40a819294599529be10accacc905da311cdc161
Jaime Rios <jaimemadrigalrios@gmail.com>
2018-05-04 07:06:03 -0500

    Added mutation for singup form.

--- client/components/signup/index.js
+++ client/components/signup/index.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import AuthForm from '../auth-form';
+import { graphql } from 'react-apollo';
+import mutation from '../../mutations/signup';
 
 class SignupForm extends Component {
+  onSubmit({ email, password}) {
+    this.props.mutate({
+      variables: { email, password }
+    })
+  }
+
   render() {
     return (
       <div  className="row">
         <h3>Sign Up</h3>
         <AuthForm
-          
+          errors={[]}
+          onSubmit={this.onSubmit.bind(this)}
         />
       </div>
     )
   }
 }
 
-export default SignupForm;
+export default graphql(mutation)(SignupForm);
