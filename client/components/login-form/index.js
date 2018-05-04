2a2bfb51dc3e774161a3f899c345042cae0085f4
Jaime Rios <jaimemadrigalrios@gmail.com>
2018-05-04 06:53:17 -0500

    Added error handling functionality.

--- client/components/login-form/index.js
+++ client/components/login-form/index.js
@@ -5,10 +5,20 @@ import { graphql } from 'react-apollo';
 import query from '../../queries/current_user';
 
 class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { errors: [] }
+  }
+
   onSubmit({email, password}) {
     this.props.mutate({
       variables: { email, password },
       refetchQueries: [{ query}],
+    }).catch( res => {
+      const errors = res.graphQLErrors.map(error => error.message);
+      this.setState({ errors });
+      ;
     });
   }
 
@@ -17,6 +27,7 @@ class LoginForm extends Component {
       <div>
         <h3> Login</h3>
         <AuthForm
+          errors={this.state.errors}
           onSubmit={this.onSubmit.bind(this)}
         />
       </div>
